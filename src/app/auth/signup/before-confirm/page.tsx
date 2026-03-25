c37c23c0e4f864a62b1af7e96842f8e3164b56c2
saku <sakuro8421@gmail.com>
2026-03-26 00:10:18 +0900

    メール送信完了ページUI調整

--- src/app/auth/signup/before-confirm/page.tsx
+++ src/app/auth/signup/before-confirm/page.tsx
@@ -1,13 +1,23 @@
-import PageTitle from "@/components/PageTitle";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 // メール送信完了画面
 export default function BeforeConfirmPage() {
   return (
-    <>
-      <PageTitle title="メール送信完了" />
-      <p>
-        メールを送信しました。メールのリンクをクリックして登録を完了してください。
-      </p>
-    </>
+    <div className="flex w-full items-center justify-center p-6 md:p-10 md:mt-40">
+      <div className="w-full max-w-xl">
+        <Card className="font-zen-maru-gothic">
+          <CardHeader>
+            <CardTitle className="flex justify-center text-2xl font-bold">
+              メール送信完了
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-center text-sm leading-8">
+              メールを送信しました。<br />受信ボックスのリンクをクリックして登録を完了してください。
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 }
